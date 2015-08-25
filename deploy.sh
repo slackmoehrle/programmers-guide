7a1f6c1711b7e0ea20f5322d1fa4f0f0a3c3a870
slackmoehrle <slackmoehrle@gmail.com>
2015-08-25 17:00:20 -0400

    removed unneeded copy of files since updating theme

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ echo "run the build.sh script just in case..."
 ./build.sh
 
 echo 'applying custom changes used only for production...'
-cp search.html site/.
-cp tipuesearch.js site/js/tipuesearch/.
+#cp search.html site/.
+#cp tipuesearch.js site/js/tipuesearch/.
 
 echo "deploying site/ via rsync..."
 rsync -au site/* docops@cocos2dx:programmersguide/.
